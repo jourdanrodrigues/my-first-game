75370da00389b9ffb3fe43672adb24d7e414459f
Thiago Jourdan <thiagojourdan@gmail.com>
2021-12-26 23:57:52 -0300

    :sparkles: Detect launching and stillness

--- Assets/GreenBird/GreenBird.cs
+++ Assets/GreenBird/GreenBird.cs
@@ -6,8 +6,11 @@ namespace GreenBird
     public class GreenBird : MonoBehaviour
     {
         private Vector3 _initialPosition;
-        [SerializeField] private float launchPower = 500;
-        
+        private bool _wasLaunched;
+        private float _timeStill;
+
+        [SerializeField] private float launchPower = 250;
+
         private void Awake()
         {
             _initialPosition = transform.position;
@@ -15,21 +18,39 @@ namespace GreenBird
 
         private void Update()
         {
-            if (transform.position.y is > 10 or < -10 || transform.position.x is > 10 or < -10)
-            {
+            TrackStillness();
+            
+            if (ShouldReloadScene())
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
+        }
+        
+        private void TrackStillness()
+        {
+            if (!_wasLaunched) return;
+            if (GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+                _timeStill += Time.deltaTime;
+            else
+                _timeStill = 0;
         }
 
+        private bool ShouldReloadScene()
+        {
+            var position = transform.position;
+            return (position.y is > 10 or < -10 || position.x is > 10 or < -10 || _timeStill > 3);
+        }
+        
         private void OnMouseUp()
         {
             Vector2 toDirection = _initialPosition - transform.position;
             var rigidBody = GetComponent<Rigidbody2D>();
             rigidBody.AddForce(toDirection * launchPower);
             rigidBody.gravityScale = 1;
+            _wasLaunched = true;
         }
+        
         private void OnMouseDrag()
         {
+            if (Camera.main == null) return;
             var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
         }
