84b2d36e3b4b51db503211c0c9a85765cdcb9adf
Thiago Jourdan <thiagojourdan@gmail.com>
2021-12-29 13:53:54 -0300

    :sparkles: Add particles on enemy killing

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -4,11 +4,13 @@ namespace Enemy
 {
     public class Enemy : MonoBehaviour
     {
+        [SerializeField] private GameObject _cloudParticlesPrefab;
         private void OnCollisionEnter2D(Collision2D collision)
         {
             var wasHitByBird = collision.collider.GetComponent<GreenBird.GreenBird>() != null;
             var wasHitFromTop = collision.contacts[0].normal.y < -0.5;
             if (!(wasHitByBird || wasHitFromTop)) return;
+            Instantiate(_cloudParticlesPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
