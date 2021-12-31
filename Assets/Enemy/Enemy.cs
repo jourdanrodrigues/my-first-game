6ff3ceed3c13517b9c18e94406739978c3021593
Thiago Jourdan <thiagojourdan@gmail.com>
2021-12-31 16:13:55 -0300

    :pencil: Rename "Enemy._cloudParticlesPrefab" to "cloudParticlesPrefab"

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Enemy
 {
     public class Enemy : MonoBehaviour
     {
-        [SerializeField] private GameObject _cloudParticlesPrefab;
+        [SerializeField] private GameObject cloudParticlesPrefab;
         private void OnCollisionEnter2D(Collision2D collision)
         {
             var wasHitByBird = collision.collider.GetComponent<GreenBird.GreenBird>() != null;
             var wasHitFromTop = collision.contacts[0].normal.y < -0.5;
             if (!(wasHitByBird || wasHitFromTop)) return;
-            Instantiate(_cloudParticlesPrefab, transform.position, Quaternion.identity);
+            Instantiate(cloudParticlesPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
